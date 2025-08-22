065e0d2c33fd07025bce973e4f8a3506203471c6
이연우 <yeunwoo217@gmail.com>
2025-08-22 13:48:49 +0900

    Lv 1-3: Validation

--- src/main/java/org/example/expert/domain/user/service/UserService.java
+++ src/main/java/org/example/expert/domain/user/service/UserService.java
@@ -9,6 +9,8 @@ import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @RequiredArgsConstructor
@@ -24,24 +26,22 @@ public class UserService {
     }
 
     @Transactional
-    public void changePassword(long userId, UserChangePasswordRequest userChangePasswordRequest) {
-        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
-                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
-                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
-            throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
-        }
-
+    public void changePassword(long userId, @Validated @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
+        // 사용자 조회 (ID로 사용자 찾기), 사용자 없으면 예외 발생
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new InvalidRequestException("User not found"));
 
+        // 새 비밀번호가 기존 비밀번호와 같을 수 없다는 조건을 확인
         if (passwordEncoder.matches(userChangePasswordRequest.getNewPassword(), user.getPassword())) {
             throw new InvalidRequestException("새 비밀번호는 기존 비밀번호와 같을 수 없습니다.");
         }
 
+        // 사용자가 입력한 기존 비밀번호가 현재 비밀번호와 일치하는지 확인
         if (!passwordEncoder.matches(userChangePasswordRequest.getOldPassword(), user.getPassword())) {
             throw new InvalidRequestException("잘못된 비밀번호입니다.");
         }
 
+        // 비밀번호 변경: 새 비밀번호를 암호화하여 변경
         user.changePassword(passwordEncoder.encode(userChangePasswordRequest.getNewPassword()));
     }
 }
